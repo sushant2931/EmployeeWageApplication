ef16eef061498860d2e3a5cbdda99944685394bc
sushant2931 <sushantlad8888@gmail.com>
2021-10-08 13:19:17 +0530

    ADDED : Check Employee Attendence Using If Else

--- Empwageapplication.js
+++ Empwageapplication.js
@@ -1,1 +1,10 @@
 console.log("********Welcome in Emplyoee Wage Application *******")
+//UC1
+const IS_ABSENT = 0
+let employeeCheck = Math.floor(Math.random() * 10) % 2;
+
+if (employeeCheck == IS_ABSENT) {
+    console.log("Employee is ABSENT");
+} else {
+    console.log("Employee is PRESENT");
+}
