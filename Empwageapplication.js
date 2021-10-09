b27d7b31927db20b23b20119bc11763edb036902
sushant2931 <sushantlad8888@gmail.com>
2021-10-10 01:12:31 +0530

    REFACTOR : Calculate Wage Using While Loop

--- Empwageapplication.js
+++ Empwageapplication.js
@@ -1,17 +1,18 @@
 console.log("********Welcome in Emplyoee Wage Application *******")
 
-
-//UC4
-
+//UC5
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 3;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HOURS_IN_MONTH = 100;
 
 let employeeHours = 0;
+let totalEmployeeHours = 0;
+let totalWorkingDays = 0;
 
 function getWorkingHours(employeeCheck) {
 
@@ -31,10 +32,13 @@ function getWorkingHours(employeeCheck) {
     }
 }
 
-for(let day = 0; day<NUM_OF_WORKING_DAYS; day++){
+while(totalEmployeeHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     employeeCheck = Math.floor(Math.random() * 10) % 3;
-    employeeHours += getWorkingHours(employeeCheck);
+    totalEmployeeHours += getWorkingHours(employeeCheck);
 }
 
-let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Total Working Hours = "+ employeeHours + "\nEmployee Wage = " + employeeWage);
+let employeeWage = totalEmployeeHours * WAGE_PER_HOUR;
+console.log("\nTotal Working Days = " + totalWorkingDays + "\nTotal Working Hours = " + totalEmployeeHours + "\nEmployee Wage = " + employeeWage);
+
+
