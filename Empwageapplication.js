0406f71bb65bfa66f5268b129f7bc96c3f8b1aac
sushant2931 <sushantlad8888@gmail.com>
2021-10-10 01:06:56 +0530

    REFACTOR : Calculate Working Days Using For Loop

--- Empwageapplication.js
+++ Empwageapplication.js
@@ -1,32 +1,40 @@
 console.log("********Welcome in Emplyoee Wage Application *******")
 
 
-//UC3
+//UC4
+
+const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 3;
 
 let employeeHours = 0;
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
 
-function getWorkingHours(employeeCheck){
+function getWorkingHours(employeeCheck) {
 
     switch (employeeCheck) {
-    
+
         case IS_PART_TIME:
             console.log("Employee works Part Time");
             return PART_TIME_HOURS;
-            
+
         case IS_FULL_TIME:
             console.log("Employee works Full Time");
             return FULL_TIME_HOURS;
-            
+
         default:
+            console.log("Employee is ABSENT");
             return 0;
     }
 }
-employeeHours = getWorkingHours(employeeCheck);
+
+for(let day = 0; day<NUM_OF_WORKING_DAYS; day++){
+    employeeCheck = Math.floor(Math.random() * 10) % 3;
+    employeeHours += getWorkingHours(employeeCheck);
+}
+
 let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Employee Wage = " + employeeWage);
+console.log("Total Working Hours = "+ employeeHours + "\nEmployee Wage = " + employeeWage);
