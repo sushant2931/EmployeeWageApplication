b6d95498e09492e087552ccb305b716cc9384412
sushant2931 <sushantlad8888@gmail.com>
2021-10-10 01:01:47 +0530

    REFACTOR : Write A function to getting Work Hours

--- Empwageapplication.js
+++ Empwageapplication.js
@@ -1,8 +1,7 @@
 console.log("********Welcome in Emplyoee Wage Application *******")
 
 
-//UC2
-
+//UC3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -12,21 +11,22 @@ const WAGE_PER_HOUR = 20;
 let employeeHours = 0;
 let employeeCheck = Math.floor(Math.random() * 10) % 3;
 
-switch (employeeCheck) {
+function getWorkingHours(employeeCheck){
 
-    case IS_PART_TIME:
-        employeeHours = PART_TIME_HOURS;
-        console.log("Employee works Part Time");
-        break;
-
-    case IS_FULL_TIME:
-        employeeHours = FULL_TIME_HOURS;
-        console.log("Employee works Full Time");
-        break;
-
-    default:
-        employeeHours = 0;
+    switch (employeeCheck) {
+    
+        case IS_PART_TIME:
+            console.log("Employee works Part Time");
+            return PART_TIME_HOURS;
+            
+        case IS_FULL_TIME:
+            console.log("Employee works Full Time");
+            return FULL_TIME_HOURS;
+            
+        default:
+            return 0;
+    }
 }
-
+employeeHours = getWorkingHours(employeeCheck);
 let employeeWage = employeeHours * WAGE_PER_HOUR;
 console.log("Employee Wage = " + employeeWage);
